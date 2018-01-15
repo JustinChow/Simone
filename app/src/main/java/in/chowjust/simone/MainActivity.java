672fba4a8d297ca49d2ac71d0c93f9fc30218504
Justin Chow <justin.chow@queensu.ca>
2018-01-14 23:21:29 -0500

    Adding buttons in main activity. Also adding functionality to 'Create Hotspot' button to open the tethering/mobile hotspot options

--- app/src/main/java/in/chowjust/simone/MainActivity.java
+++ app/src/main/java/in/chowjust/simone/MainActivity.java
@@ -1,7 +1,10 @@
 package in.chowjust.simone;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openHotspotSettings(View view) {
+        final Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        final ComponentName cn = new ComponentName("com.android.settings", "com.android.settings.TetherSettings");
+        intent.setComponent(cn);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity( intent);
     }
 }
